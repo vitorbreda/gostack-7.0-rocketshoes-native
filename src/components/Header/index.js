d4f35b3c2a974ee0e7fc0f44a95100f139b96679
Vitor Breda <vitor@olivasdigital.com.br>
2019-12-03 00:36:36 -0300

    first commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import {Wrapper, Container, Logo, ItemCount, BasketContainer} from './styles';
+
+export default function Header({navigation}) {
+    return (
+        <Wrapper>
+            <Container>
+                <Logo />
+                <BasketContainer onPress={() => navigation.navigate('Cart')}>
+                    <Icon name="shopping-basket" color="#FFF" size={24} />
+                    <ItemCount>3</ItemCount>
+                </BasketContainer>
+            </Container>
+        </Wrapper>
+    );
+}
---
+++ src/pages/Main/index.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+import {View, Text} from 'react-native';
+
+// import { Container } from './styles';
+
+export default class Main extends Component {
+    state = {
+        products: [],
+    };
+
+    render() {
+        return (
+            <View>
+                <Text>HOme</Text>
+            </View>
+        );
+    }
+}
