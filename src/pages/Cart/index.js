aaf8f18fc26ed82bb711ee176a41b4c39822f0be
Vitor Breda <vitor@olivasdigital.com.br>
2019-12-08 23:24:55 -0300

    Main and Car Screen

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,12 +1,90 @@
 import React from 'react';
-import {View, Text} from 'react-native';
 
-// import { Container } from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import colors from '../../styles/colors';
+
+import {
+    Container,
+    CartBox,
+    Product,
+    ProductInfo,
+    ProductImage,
+    ProductDetails,
+    ProductTitle,
+    ProductPrice,
+    ProductDelete,
+    ProductControls,
+    ProductControlButton,
+    ProductAmount,
+    ProductSubtotal,
+    TotalContainer,
+    TotalText,
+    TotalAmount,
+    OrderButton,
+    OrderButtonText,
+    EmptyContainer,
+    EmptyText,
+} from './styles';
 
 export default function Cart() {
     return (
-        <View>
-            <Text>Cart</Text>
-        </View>
+        <Container>
+            <CartBox>
+                <Product>
+                    <ProductInfo>
+                        <ProductImage
+                            source={{
+                                uri:
+                                    'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg',
+                            }}
+                        />
+                        <ProductDetails>
+                            <ProductTitle>
+                                Tênis VR Caminhada Confortável
+                            </ProductTitle>
+                            <ProductPrice>R$ 179,90</ProductPrice>
+                        </ProductDetails>
+                        <ProductDelete>
+                            <Icon
+                                name="delete-forever"
+                                size={24}
+                                color={colors.primary}
+                            />
+                        </ProductDelete>
+                    </ProductInfo>
+                    <ProductControls>
+                        <ProductControlButton>
+                            <Icon
+                                name="remove-circle-outline"
+                                size={20}
+                                color={colors.primary}
+                            />
+                        </ProductControlButton>
+                        <ProductAmount value="2" />
+                        <ProductControlButton>
+                            <Icon
+                                name="add-circle-outline"
+                                size={20}
+                                color={colors.primary}
+                            />
+                        </ProductControlButton>
+                        <ProductSubtotal>R$ 1000,50</ProductSubtotal>
+                    </ProductControls>
+                </Product>
+                <TotalContainer>
+                    <TotalText>TOTAL</TotalText>
+                    <TotalAmount>R$ 2000.00</TotalAmount>
+                    <OrderButton>
+                        <OrderButtonText>FINALIZAR PEDIDO</OrderButtonText>
+                    </OrderButton>
+                </TotalContainer>
+            </CartBox>
+        </Container>
+
+        // <EmptyContainer>
+        //     <Icon name="remove-shopping-cart" size={64} color="#fff" />
+        //     <EmptyText>Seu carrinho está vazio.</EmptyText>
+        // </EmptyContainer>
     );
 }
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,19 +1,62 @@
 import React, {Component} from 'react';
 
-import {View, Text} from 'react-native';
+import {FlatList} from 'react-native-gesture-handler';
 
-// import { Container } from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import api from '../../services/api';
+
+import {
+    Container,
+    Product,
+    ProductImage,
+    ProductTitle,
+    ProductPrice,
+    AddButton,
+    ProductAmount,
+    ProductAmountText,
+    AddButtonText,
+} from './styles';
 
 export default class Main extends Component {
     state = {
         products: [],
     };
 
-    render() {
+    async componentDidMount() {
+        const response = await api.get('/products');
+
+        this.setState({products: response.data});
+    }
+
+    renderProduct = ({item}) => {
         return (
-            <View>
-                <Text>HOme</Text>
-            </View>
+            <Product>
+                <ProductImage source={{uri: item.image}} />
+                <ProductTitle>{item.title}</ProductTitle>
+                <ProductPrice>{item.price}</ProductPrice>
+                <AddButton>
+                    <ProductAmount>
+                        <Icon name="add-shopping-cart" color="#fff" size={20} />
+                        <ProductAmountText>1</ProductAmountText>
+                    </ProductAmount>
+                    <AddButtonText>ADICIONAR</AddButtonText>
+                </AddButton>
+            </Product>
+        );
+    };
+
+    render() {
+        const {products} = this.state;
+        return (
+            <Container>
+                <FlatList
+                    horizontal
+                    data={products}
+                    keyExtractor={item => String(item.id)}
+                    renderItem={this.renderProduct}
+                />
+            </Container>
         );
     }
 }
